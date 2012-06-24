72c7a99a883be569741ff3dabb6f14bbcd05bfe1
Erasmo Leal <erasmo_eli@yahoo.com.mx>
2012-06-23 20:14:03 -0500

    Actualización a 1.0.1

--- Tienda_Trivial/web/addTables.sql
+++ Tienda_Trivial/web/addTables.sql
@@ -1,6 +1,16 @@
 /*IMPORTANTE: Se debe de crear la base de datos "tiendatrivial" antes de
 implementar el codigo. Posteriormente, se ejecuta este codigo dentro de ella.
-V 1.0*/
+V 1.0.1*/
+
+CREATE TABLE Usuario (idusuario varchar(15) not null,
+					  nombre varchar(30) not null,
+					  apellido varchar(40) not null,
+					  password varchar(10) not null,
+					  nivelacceso char(1) not null,
+					  correo varchar(30) not null,
+					  intentosfallidos char(1) not null,
+					  Primary Key(idusuario));
+					  
 
 CREATE TABLE Cliente (idcliente char(10) not null,
 					  nombre varchar(30) not null,
@@ -31,21 +41,28 @@ CREATE TABLE Producto (upc varchar(10) not null,
 CREATE TABLE Venta (idventa char(10) not null,
 					 idcliente char(10) not null,
 					 upc varchar(10) not null,
+					 idusuario varchar(15) not null,
 					 cantidad int not null,
 					 fechaventa datetime not null,
-					 Primary Key(idventa),
+					 Primary Key(idventa, idcliente, upc),
 					 Foreign Key(idcliente) references Cliente(idcliente),
-					 Foreign Key(upc) references Producto(upc));
+					 Foreign Key(upc) references Producto(upc),
+					 Foreign Key(idusuario) references Usuario(idusuario));
+					 
 
 CREATE TABLE Devolucion	(iddevolucion char(10) not null,
 						 idcliente char(10) not null,
 						 idventa char(10) not null,
 						 upc char(10) not null,
+						 idusuario varchar(15) not null,
 						 cantidad int not null,
 						 total int not null,
 						 fechadevolucion datetime not null,
-						 Primary Key(iddevolucion),
+						 defectuoso boolean not null,
+						 razon text null,
+						 Primary Key(iddevolucion, idcliente, idventa, upc),
 						 Foreign Key(idcliente) references Cliente(idcliente),
 						 Foreign Key(idventa) references Venta(idventa),
-						 Foreign Key(upc) references Producto(upc));
-						 
+						 Foreign Key(upc) references Producto(upc),
+						 Foreign Key(idusuario) references Usuario(idusuario));
+						 						 

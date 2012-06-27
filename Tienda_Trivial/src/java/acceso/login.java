56581386e3942279178b85bf077bd40120fd84cf
Erasmo Leal <erasmo_eli@yahoo.com.mx>
2012-06-26 20:49:59 -0500

    sincronizacion

--- Tienda_Trivial/src/java/acceso/login.java
+++ Tienda_Trivial/src/java/acceso/login.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package acceso;
+package acceso; 
 
 import arquetipo.Usuario;
 import java.io.IOException;
